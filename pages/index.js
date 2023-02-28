4f4f6976577f681953250e247c8a12513521048d
kul-sudo <ulyanov.kirill@protonmail.com>
2023-02-28 22:41:14 +0300

    feat: added cross for keyboard overlay

--- pages/index.js
+++ pages/index.js
@@ -246,6 +246,16 @@ const Home = () => {
                   </g>
                 </svg>
               </div>
+              <button onClick={() => setKeyboardToggle(false)}>
+                <div className="fixed top-5 right-5">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="flex-shrink-0 fill-current object-contain h-6 w-6 text-gray-700 dark:text-gray-300">
+                    <path
+                      fill="currentColor"
+                      d="M.439,21.44a1.5,1.5,0,0,0,2.122,2.121L11.823,14.3a.25.25,0,0,1,.354,0l9.262,9.263a1.5,1.5,0,1,0,2.122-2.121L14.3,12.177a.25.25,0,0,1,0-.354l9.263-9.262A1.5,1.5,0,0,0,21.439.44L12.177,9.7a.25.25,0,0,1-.354,0L2.561.44A1.5,1.5,0,0,0,.439,2.561L9.7,11.823a.25.25,0,0,1,0,.354Z"
+                      />
+                  </svg>
+                </div>
+              </button>
             </div>
           </Center>
           
@@ -268,7 +278,7 @@ const Home = () => {
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
           <div className="flex justify-center items-center bg-zinc-900 w-[30rem] h-[25rem] rounded-xl border-[1px] border-[#fff] border-opacity-[0.16]">
             <Center>
-              <button onClick={() => setAddWord(!addWord)}>
+              <button onClick={() => setAddWord(false)}>
                 <div className="fixed top-5 right-5">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="flex-shrink-0 fill-current object-contain h-6 w-6 text-gray-700 dark:text-gray-300">
                     <path
